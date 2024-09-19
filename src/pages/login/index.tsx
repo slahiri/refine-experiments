e3040d01d53839f1f7ba4563071aeddbea0011f5
Siddhartha Lahiri <siddhartha@valuefy.com>
2024-09-19 23:52:27 +0530

    initial commit

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,12 @@
-import {
-    AuthPage,
-} from "@refinedev/antd";
+import { AuthPage } from "@refinedev/antd";
 
 export const Login = () => {
-    return (
-        <AuthPage
-            type="login"
-            formProps={{ initialValues:{ email: "demo@refine.dev", password: "demodemo" } }}
-        />
-    );
+  return (
+    <AuthPage
+      type="login"
+      formProps={{
+        initialValues: { email: "demo@refine.dev", password: "demodemo" },
+      }}
+    />
+  );
 };
